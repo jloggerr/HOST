9b9353e8df74284921591cc7162031fe7d50e260
jlogger <33246273+jloggerr@users.noreply.github.com>
2017-10-31 10:06:57 +0330

    Create host.py

---
+++ host.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import socket
+
+print ""
+   HOST Scanner
+      ""
+
+r = str(raw_input("IP >>> ")
+t = socket.gethostbyname(r)
+s = socket.getfdn(r)
+
+print ""
+IP => %s
+HOST => %s
+      ""% (t, s)
+      
